f61607ef98d3cf531fabccaf18ef9506e70a9ce8
ElsaiedSamaka <elsaied.samaka1997@gmail.com>
2020-05-20 09:58:16 +0200

    final commit

---
+++ my_awosome_vedios/lib/chewie_list_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
+
+class ChewieItem extends StatefulWidget {
+  final VideoPlayerController videoPlayerController;
+  final bool isLoop;
+  ChewieItem({@required this.videoPlayerController, this.isLoop, Key key})
+      : super(key: key);
+  @override
+  _ChewieItemState createState() => _ChewieItemState();
+}
+
+class _ChewieItemState extends State<ChewieItem> {
+  ChewieController _chewieController;
+  @override
+  void initState() {
+    super.initState();
+    _chewieController = ChewieController(
+      videoPlayerController: widget.videoPlayerController,
+      looping: widget.isLoop,
+      aspectRatio: 16 / 9,
+      autoInitialize: true,
+      errorBuilder: (context, errorMessage) {
+        return Center(
+          child: Text(
+            '$errorMessage',
+            style: TextStyle(color: Colors.teal),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Chewie(
+        controller: _chewieController,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    widget.videoPlayerController.dispose();
+    _chewieController.dispose();
+  }
+}
